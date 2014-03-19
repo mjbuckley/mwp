f9d5fcd8fb37e35b145e42ed719872680d0a434e
Michael Buckley <michaelbuckleyis@gmail.com>
2014-03-18 22:25:24 -0400

    Finish sign in and fix gemfile

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   shared_examples_for "all static pages" do
     it { should have_selector('h1', text: heading) }     
     it { should have_title(full_title(page_title)) }
-    end
+  end
 
   
   describe "Home page" do
